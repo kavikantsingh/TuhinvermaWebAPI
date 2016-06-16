068f6be3d093b09bfc8854c422287e2578900a00
unknown <basu.sharma@inlumon.com>
2016-06-16 12:21:18 +0530

    Added methods in region ProviderDocument, Added required DAL-BAL's and entities.

---
+++ LAPP.ENTITY/DocumentMaster.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAPP.ENTITY
+{
+    public class DocumentMaster : BaseEntity
+    {
+        public int DocumentMasterId { get; set; }
+        public int DocumentId { get; set; }
+        public string DocumentCd { get; set; }
+        public string DocumentName { get; set; }
+        public int DocumentTypeId { get; set; }
+        public string DocumentTypeIdName { get; set; }
+        public string DocumentTypeDesc { get; set; }
+        public int Max_size { get; set; }
+        public int MasterTransactionId { get; set; }
+        public int PageModuleId { get; set; }
+        public int PageModuleTabSubModuleId { get; set; }
+        public int PageTabSectionId { get; set; }
+        public DateTime EffectiveDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsEnabled { get; set; }
+        public bool IsEditable { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+    }
+
+    public class DocumentMasterResponse : BaseEntityServiceResponse
+    {
+        public List<DocumentMaster> DocumentMaster { get; set; }
+    }
+
+    public class DocumentMasterGET : BaseEntity
+    {
+        public int DocumentTypeId { get; set; }
+        public string DocumentTypeIdName { get; set; }
+        public string DocumentTypeDesc { get; set; }
+        public int Max_size { get; set; }
+    }
+
+    public class DocumentMasterGETResponse : BaseEntityServiceResponse
+    {
+        public List<DocumentMasterGET> DocumentMasterGET { get; set; }
+    }
+}
---
+++ LAPP.WS/Controllers/Backoffice/DocumentController.cs
@@ -0,0 +1,87 @@
+﻿using LAPP.BAL;
+using LAPP.ENTITY;
+using LAPP.ENTITY.Enumeration;
+using LAPP.LOGING;
+using LAPP.WS.App_Helper;
+using LAPP.WS.App_Helper.Common;
+using LAPP.WS.ValidateController.Backoffice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace LAPP.WS.Controllers.Backoffice
+{
+    public class DocumentController : ApiController
+    {
+        /// <summary>
+        /// Get Method to get DocumentTypeName by key, DocumentId and DocumentCode.
+        /// </summary>
+        /// <param name="Key">API security key.</param>
+        /// <param name="DocId">Document ID.</param>
+        /// <param name="DocCode">Document Code.</param>
+        [AcceptVerbs("GET")]
+        [ActionName("DocumentGetDocumentTypeName")]
+        public DocumentMasterGETResponse DocumentGetDocumentTypeName(string Key, int DocId, string DocCode)
+        {
+            LogingHelper.SaveAuditInfo(Key);
+
+            DocumentMasterGETResponse objResponse = new DocumentMasterGETResponse();
+            DocumentMasterBAL objBAL = new DocumentMasterBAL();
+            DocumentMasterGET objEntity = new DocumentMasterGET();
+            List<DocumentMasterGET> lstDocumentMasterGET = new List<DocumentMasterGET>();
+
+            try
+            {
+                if (!TokenHelper.ValidateToken(Key))
+                {
+                    objResponse.Status = false;
+                    objResponse.StatusCode = Convert.ToInt32(ResponseStatusCode.ValidateToken).ToString("00");
+                    objResponse.Message = "User session has expired.";
+                    objResponse.DocumentMasterGET = null;
+                    return objResponse;
+                }
+                lstDocumentMasterGET = objBAL.Get_DocumentMaster_By_DocId_And_DocCode(DocId, DocCode);
+                if (lstDocumentMasterGET != null && lstDocumentMasterGET.Count > 0)
+                {
+                    objResponse.ResponseReason = "To Get All Page Modules";
+                    objResponse.Status = true;
+                    objResponse.Message = "";
+                    objResponse.StatusCode = Convert.ToInt32(ResponseStatusCode.Success).ToString("00");
+
+                    var lstDocumentMasterGETSelected = lstDocumentMasterGET.Select(DocMaster => new DocumentMasterGET
+                    {
+                        DocumentTypeId = DocMaster.DocumentTypeId,
+                        DocumentTypeIdName = DocMaster.DocumentTypeIdName,
+                        DocumentTypeDesc = DocMaster.DocumentTypeDesc,
+                        Max_size = DocMaster.Max_size
+                    }
+                    ).ToList();
+
+                    objResponse.DocumentMasterGET = lstDocumentMasterGETSelected;
+                }
+                else
+                {
+                    objResponse.Status = false;
+                    objResponse.Message = "No record found.";
+                    objResponse.StatusCode = Convert.ToInt32(ResponseStatusCode.Success).ToString("00");
+                    objResponse.DocumentMasterGET = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                LogingHelper.SaveExceptionInfo(Key, ex, "DocumentGetDocumentTypeName", ENTITY.Enumeration.eSeverity.Error);
+
+                objResponse.Status = false;
+                objResponse.StatusCode = Convert.ToInt32(ResponseStatusCode.Exception).ToString("00");
+                objResponse.Message = ex.Message;
+                objResponse.DocumentMasterGET = null;
+            }
+
+            return objResponse;
+        }
+    }
+}
